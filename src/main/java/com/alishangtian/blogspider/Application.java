f50a72f316be051c6625e927762375655e508d45
maoxiaobing <maoxiaobing@xiaomi.com>
2020-04-24 08:25:45 +0800

    init project

---
+++ src/main/java/com/alishangtian/blogspider/Application.java
@@ -0,0 +1,18 @@
+package com.alishangtian.blogspider;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author maoxiaobing
+ */
+@SpringBootApplication
+@Log4j2
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
