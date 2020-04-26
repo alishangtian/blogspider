1b0ef4578e9c68b71bd32ac5567e4795a50419d2
maoxiaobing <maoxiaobing@xiaomi.com>
2020-04-26 11:14:19 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 git pull origin master
 mvn clean package
-PID=$(ps aux|grep blogspider-1.0-SNAPSHOT|grep -v grep|awk '{print $2}')
-echo "PID:$PID"
-kill -9 "$PID"
+OLDPID=$(ps aux|grep blogspider-1.0-SNAPSHOT|grep -v grep|awk '{print $2}')
+echo "OLDPID:$OLDPID"
+kill -9 "$OLDPID"
 nohup java -jar ./target/blogspider-1.0-SNAPSHOT.jar &
+NEWPID=$(ps aux|grep blogspider-1.0-SNAPSHOT|grep -v grep|awk '{print $2}')
+echo "NEWPID:$NEWPID"
