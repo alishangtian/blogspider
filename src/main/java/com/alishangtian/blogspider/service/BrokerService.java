24901d8a4003b376ef47920720168c2b59e80df0
maoxiaobing <maoxiaobing@xiaomi.com>
2020-05-01 13:29:02 +0800

    添加超时监控timer

--- src/main/java/com/alishangtian/blogspider/service/BrokerService.java
+++ src/main/java/com/alishangtian/blogspider/service/BrokerService.java
@@ -5,6 +5,9 @@ import com.alishangtian.blogspider.cluster.Node;
 import com.alishangtian.blogspider.remoting.Remoting;
 import com.alishangtian.blogspider.util.JSONUtils;
 import com.google.gson.reflect.TypeToken;
+import io.netty.util.HashedWheelTimer;
+import io.netty.util.Timeout;
+import io.netty.util.TimerTask;
 import lombok.extern.log4j.Log4j2;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,9 +46,16 @@ public class BrokerService {
             return new Thread(r, "broker-ping-thread-" + num.getAndIncrement());
         }
     });
-
-    private ConcurrentMap<String, Node> activeNodes;
+    private ConcurrentMap<String, Node> activeNodes = new ConcurrentHashMap<>();
     private ConcurrentMap<String, Node> outNodes = new ConcurrentHashMap<>();
+    private HashedWheelTimer timer = new HashedWheelTimer(new ThreadFactory() {
+        AtomicLong num = new AtomicLong();
+
+        @Override
+        public Thread newThread(@NotNull Runnable r) {
+            return new Thread(r, "hashed-wheel-timer-thread-" + num.getAndIncrement());
+        }
+    });
 
     @PostConstruct
     public void init() {
@@ -57,6 +67,8 @@ public class BrokerService {
     }
 
     /**
+     * 执行ping
+     *
      * @Author maoxiaobing
      * @Description ping
      * @Date 2020/4/28
@@ -65,18 +77,29 @@ public class BrokerService {
      */
     public void ping() {
         activeNodes.forEach((s, node) -> {
-            if (!s.equals(brokerProperties.getSelfServer())) {
-                log.info("ping {}", s);
-                threadPoolExecutor.submit(() -> {
-                    if (!Remoting.ping(s, activeNodes.values(), brokerProperties.getSelfServer(), brokerProperties.getPort())) {
-                        activeNodes.remove(s);
-                    }
-                });
-            }
+            pingServer(s, node);
         });
     }
 
+    private void pingServer(String s, Node node) {
+        if (!s.equals(brokerProperties.getSelfServer())) {
+            log.info("ping {}", s);
+            threadPoolExecutor.submit(() -> {
+                if (!Remoting.ping(s, activeNodes.values(), brokerProperties.getSelfServer(), brokerProperties.getPort())) {
+                    activeNodes.remove(s);
+                    outNodes.putIfAbsent(s, node);
+                    timer.newTimeout(timeout -> pingServer(s, node), 5, TimeUnit.SECONDS);
+                } else {
+                    activeNodes.putIfAbsent(s, node);
+                    outNodes.remove(s);
+                }
+            });
+        }
+    }
+
     /**
+     * 回复ping
+     *
      * @Author maoxiaobing
      * @Description pong
      * @Date 2020/4/30
@@ -87,7 +110,10 @@ public class BrokerService {
         List<Node> nodes = JSONUtils.parseObject(body, new TypeToken<List<Node>>() {
         }.getType());
         nodes.add(new Node(server, port));
-        nodes.forEach(node -> activeNodes.putIfAbsent(node.getServer(), node));
+        nodes.forEach(node -> {
+            activeNodes.putIfAbsent(node.getServer(), node);
+            outNodes.remove(node.getServer());
+        });
         return "pong";
     }
 }
